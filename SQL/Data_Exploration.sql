4c2c9cca047379d32951edcc73fb848ad424e80e
Priya Vishwakarma <vishpriya.official@gmail.com>
2026-06-29 15:43:40 +0530

    Refactor SQL queries for clarity and organization

--- SQL/Data_Exploration.sql
+++ SQL/Data_Exploration.sql
@@ -1,2 +1,20 @@
-select count(*) AS total_jobs from ai_job_market_insights;
-Select distinct industry from ai_job_market_insights;
+-- Business Question 1
+-- How many job records are available?
+
+SELECT COUNT(*) AS Total_Jobs
+FROM ai_job_market_insights;
+
+-- Business Question 2
+-- Which industries are available?
+
+SELECT DISTINCT Industry
+FROM ai_job_market_insights
+ORDER BY Industry;
+
+-- Business Question 3
+-- How many jobs are available in each industry?
+
+SELECT Industry,
+COUNT(*) AS Total_Jobs
+FROM ai_job_market_insights
+GROUP BY Industry;
