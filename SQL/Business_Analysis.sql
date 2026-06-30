04bddbd88326e4bef79d00b78b16c9c26e6ac46e
Priya Vishwakarma <vishpriya.official@gmail.com>
2026-06-30 18:12:29 +0530

    Add SQL queries for AI job market analysis
    Added SQL queries for business analysis on AI job postings, including total job count, industry presence, job count by industry, job count by location, and salary distribution.

--- SQL/Business_Analysis.sql
+++ SQL/Business_Analysis.sql
@@ -1,1 +1,54 @@
+-- =====================================================
+-- Business Analysis
+-- Project: AI Adoption and Workforce Dashboard Analytics
+-- =====================================================
 
+-- Business Question 1
+-- How many AI-related job postings are available in the dataset?
+
+SELECT COUNT(*) AS Total_Jobs
+FROM ai_job_market_insights;
+
+
+-- Business Question 2
+-- Which industries are present in the dataset?
+
+SELECT DISTINCT Industry
+FROM ai_job_market_insights
+ORDER BY Industry;
+
+
+-- Business Question 3
+-- How many jobs are available in each industry?
+
+SELECT Industry,
+COUNT(*) AS Total_Jobs
+FROM ai_job_market_insights
+GROUP BY Industry
+ORDER BY Total_Jobs DESC;
+
+
+-- Business Question 4
+-- How many AI job postings are available in each location?
+
+SELECT Location,
+COUNT(*) AS Total_AI_Jobs
+FROM ai_job_market_insights
+GROUP BY Location
+ORDER BY Total_AI_Jobs DESC;
+
+
+-- Business Question 5
+-- What is the salary distribution of AI jobs?
+
+-- Average Salary
+SELECT ROUND(AVG(Salary_USD),0) AS Average_Salary
+FROM ai_job_market_insights;
+
+-- Maximum Salary
+SELECT ROUND(MAX(Salary_USD),0) AS Maximum_Salary
+FROM ai_job_market_insights;
+
+-- Minimum Salary
+SELECT ROUND(MIN(Salary_USD),0) AS Minimum_Salary
+FROM ai_job_market_insights;
