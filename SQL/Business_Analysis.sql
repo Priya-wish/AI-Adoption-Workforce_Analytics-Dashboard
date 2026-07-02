356401896e9cf57b33d1a310f19d5f7f926172a8
Priya Vishwakarma <vishpriya.official@gmail.com>
2026-07-02 16:54:46 +0530

    Add queries for AI job postings analysis
    Added queries to analyze AI job postings by adoption and automation risk levels.

--- SQL/Business_Analysis.sql
+++ SQL/Business_Analysis.sql
@@ -9,38 +9,40 @@
 SELECT COUNT(*) AS Total_Jobs
 FROM ai_job_market_insights;
 
-
+===================================================================
 -- Business Question 2
 -- Which industries are present in the dataset?
-
+===================================================================
+  
 SELECT DISTINCT Industry
 FROM ai_job_market_insights
 ORDER BY Industry;
 
-
+==================================================================
 -- Business Question 3
 -- How many jobs are available in each industry?
-
+===================================================================
+  
 SELECT Industry,
 COUNT(*) AS Total_Jobs
 FROM ai_job_market_insights
 GROUP BY Industry
 ORDER BY Total_Jobs DESC;
 
-
+=================================================================
 -- Business Question 4
 -- How many AI job postings are available in each location?
-
+==================================================================
 SELECT Location,
 COUNT(*) AS Total_AI_Jobs
 FROM ai_job_market_insights
 GROUP BY Location
 ORDER BY Total_AI_Jobs DESC;
 
-
+======================================================================
 -- Business Question 5
 -- What is the salary distribution of AI jobs?
-
+======================================================================
 -- Average Salary
 SELECT ROUND(AVG(Salary_USD),0) AS Average_Salary
 FROM ai_job_market_insights;
@@ -53,18 +55,21 @@ FROM ai_job_market_insights;
 SELECT ROUND(MIN(Salary_USD),0) AS Minimum_Salary
 FROM ai_job_market_insights;
 
+========================================================================
 --Business Question 6
 --How many AI job postings are available at each AI Adoption Level ?
-
+========================================================================
 SELECT AI_Adoption_Level,
 Count(*) AS total_Jobs
 FROM ai_job_market_insights
 GROUP BY AI_Adoption_Level
 ORDER BY Total_Jobs DESC;
 
+===========================================================================
 --Business Question 7
 --How many AI job postings are available at eacg Automation RisK Level ?
-
+===========================================================================
+  
 SELECT Automation_Risk,
 COUNT(*) AS Total_Jobs
 FROM ai_job_market_insights
