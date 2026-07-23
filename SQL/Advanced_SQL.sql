277f4cec725f13268e566673e51643597404c1f0
Priya Vishwakarma <vishpriya.official@gmail.com>
2026-07-23 14:45:43 +0530

    Refactor SQL comments and format query

--- SQL/Advanced_SQL.sql
+++ SQL/Advanced_SQL.sql
@@ -1,13 +1,10 @@
---
-======================================================================
--- Advanced Business Question 1 
--- Which industry offers the highest average salary ?
---
-=======================================================================
+-- ======================================================================
+-- Advanced Business Question 1
+-- Which industry offers the highest average salary?
+-- ======================================================================
 
 SELECT Industry,
-ROUND(AVG(Salary_USD),0) AS
-Avg_Salary 
+       ROUND(AVG(Salary_USD), 0) AS Avg_Salary
 FROM ai_job_market_insights
 GROUP BY Industry
 ORDER BY Avg_Salary DESC;
